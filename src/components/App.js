5a24bb9db0da24b62bdc6adc8aa1fbf29107f099
commit-panda <sjmanyarkiy@gmail.com>
2025-11-20 15:24:35 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+    const [dogImage, setdogImage ] =useState("");
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            setdogImage(data.message)
+            console.log(data)
+        })
+        console.log("useEffect called")
+    }, [])
+
+    if(!dogImage) return <div><p>Loading</p></div>
+
+  return (
+    <div>
+        <img src={dogImage} alt='A Random Dog'/>
+    </div>
+  )
+}
+
+export default App
+
